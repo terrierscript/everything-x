fe129696ebec5d4b98f78b0bab70c0c4870871ac
S. Suzuki <terrierscript@gmail.com>
2023-07-25 22:34:20 +0900

    responsive size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import { InputForm } from "../components/InputForm"
 export default function Page() {
   return <VStack alignItems={"center"} justify={"center"}
     px={20}
-    width="100%" height={"100vh"}>
+    width="100%"
+    height={"100vh"}>
     <HStack textAlign="center" gap={10} p={10}>
-      <Box fontSize={45}>𝔼𝕧𝕖𝕣𝕪𝕥𝕙𝕚𝕟𝕘</Box>
-      <Box fontWeight={"bold"} fontSize={50}>𝕏</Box>
+      <Box fontSize={"10vmin"}>𝔼𝕧𝕖𝕣𝕪𝕥𝕙𝕚𝕟𝕘</Box>
+      <Box fontSize={"10vmin"} fontWeight={"bold"}>𝕏</Box>
     </HStack>
     <InputForm />
   </VStack>
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -15,14 +15,19 @@ export const InputForm = () => {
     <VStack textAlign={"center"} gap={8} alignItems={"center"}>
       <Input borderRadius={40} border="#ccc solid 1px"
         color="#333"
-        px={8} placeholder="𝕀𝕟𝕡𝕦𝕥 𝕙𝕖𝕣𝕖" textAlign={"center"} fontSize={40} size={40} value={text} onChange={(e) => {
+        px={8} placeholder="𝕀𝕟𝕡𝕦𝕥 𝕙𝕖𝕣𝕖" textAlign={"center"} fontSize={"4vw"}
+        maxWidth="90vw"
+        size={40}
+        value={text} onChange={(e) => {
           setText(e.target.value)
         }} />
       <Box fontSize={40}>👇👇👇</Box>
-      <HStack justify={"center"} width={"100%"} fontSize={60} gap={10} minHeight={"1.2em"}>
-        <Box>{crossed.length > 0 ? crossed : "\u00A0"}</Box>
+      <HStack justify={"center"} maxWidth={"100%"}
+        gap={10} minHeight={"1.2em"}
+      >
+        <Box fontSize={"5vw"} >{crossed.length > 0 ? crossed : "\u00A0"}</Box>
       </HStack>
-      <Button p={8} bg="teal" width={300} color="white" borderRadius={6}
+      <Button p={8} bg="teal" maxWidth={"80vw"} width={300} color="white" borderRadius={6}
         onClick={() => {
           navigator.clipboard.writeText(crossed)
           toast({
