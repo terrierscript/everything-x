8b92502d03b47e2d2147d01b5a4f4f1112f63b82
S. Suzuki <terrierscript@gmail.com>
2023-07-25 21:22:49 +0900

    fix input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Box, Flex, HStack, Heading, Input, Text, VStack } from "@kuma-ui/core"
 import { InputForm } from "../components/InputForm"
 
 export default function Page() {
-  return <VStack alignItems={"center"} justify={"center"} width="100%" height={"100vh"}>
-    <HStack textAlign="center" gap={10} py={20}>
+  return <VStack alignItems={"center"} justify={"center"}
+    px={20}
+    width="100%" height={"100vh"}>
+    <HStack textAlign="center" gap={10} p={10}>
       <Box fontSize={45}>𝔼𝕧𝕖𝕣𝕪𝕥𝕙𝕚𝕟𝕘</Box>
       <Box fontWeight={"bold"} fontSize={50}>𝕏</Box>
     </HStack>
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -10,11 +10,12 @@ export const InputForm = () => {
   }, [text])
   return <VStack textAlign={"center"} >
     <Input borderRadius={40} border="#ccc solid 1px"
+      color="#333"
       px={8} placeholder="𝕀𝕟𝕡𝕦𝕥 𝕙𝕖𝕣𝕖" textAlign={"center"} fontSize={40} size={40} value={text} onChange={(e) => {
         setText(e.target.value)
       }} />
     <Box fontSize={40}>👇👇👇</Box>
-    <HStack justify={"center"} width={"100%"} fontSize={40} gap={10} minHeight={"1.2em"}>
+    <HStack justify={"center"} width={"100%"} fontSize={60} gap={10} minHeight={"1.2em"}>
       <Box>{crossed.length > 0 ? crossed : "\u00A0"}</Box>
     </HStack>
   </VStack>
