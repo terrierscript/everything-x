5dc44fc33bfea0f5121e98e352a9b8f7a01c357c
S. Suzuki <terrierscript@gmail.com>
2023-07-25 21:11:36 +0900

    fix input

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -9,9 +9,10 @@ export const InputForm = () => {
     return convertText(text)
   }, [text])
   return <VStack textAlign={"center"} >
-    <Input placeholder="𝕀𝕟𝕡𝕦𝕥 𝕙𝕖𝕣𝕖" textAlign={"center"} fontSize={40} size={40} value={text} onChange={(e) => {
-      setText(e.target.value)
-    }} />
+    <Input borderRadius={40} border="#ccc solid 1px"
+      px={8} placeholder="𝕀𝕟𝕡𝕦𝕥 𝕙𝕖𝕣𝕖" textAlign={"center"} fontSize={40} size={40} value={text} onChange={(e) => {
+        setText(e.target.value)
+      }} />
     <Box fontSize={40}>👇</Box>
     <HStack justify={"center"} width={"100%"} fontSize={40} gap={10} minHeight={"1.2em"}>
       <Box>{crossed.length > 0 ? crossed : "\u00A0"}</Box>
